465b04974a9b32c4b5eba050fa7ca92f3b1727f2
faerieheart <72530401+faerieheart@users.noreply.github.com>
2023-09-20 16:39:29 -0400

    Player creation screen created on this branch

--- DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/GameScreen1.java
+++ DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/GameScreen1.java
@@ -1,4 +1,18 @@
 package com.example.dungeoncrawling;
 
-public class GameScreen1 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameScreen1 extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.game_screen_1);
+        }
 }
+
+
+
