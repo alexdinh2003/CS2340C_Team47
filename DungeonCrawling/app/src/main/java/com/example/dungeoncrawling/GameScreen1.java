d7115a87f796096e68f9973ab0386b3d582c9519
Nghi Dinh <ndinh31@gatech.edu>
2023-09-26 17:06:31 -0400

    Pull the lastest verion from remote to local repo

--- DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/GameScreen1.java
+++ DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/GameScreen1.java
@@ -1,7 +1,15 @@
 package com.example.dungeoncrawling;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
 
-public class GameScreen1 {
+import androidx.appcompat.app.AppCompatActivity;
 
-
+public class GameScreen1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_screen_1);
+    }
 }
