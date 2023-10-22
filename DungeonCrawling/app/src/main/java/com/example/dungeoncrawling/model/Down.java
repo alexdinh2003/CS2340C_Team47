e99a940b117a46d361fab8c9b994afc3a7cacb47
mjones464 <mmjones281@gmail.com>
2023-10-22 15:54:52 -0400

    adds observer pattern classes

--- DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/model/Down.java
+++ DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/model/Down.java
@@ -1,5 +1,5 @@
 package com.example.dungeoncrawling.model;
-
+import com.example.dungeoncrawling.model.graphics.Sprite;
 public class Down implements DirectionStrategy {
     @Override
     public void move(Player p) {
