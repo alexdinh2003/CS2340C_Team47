965184ff1c03dd848308d3920955c84b0a8f1bdc
faerieheart <72530401+faerieheart@users.noreply.github.com>
2023-10-31 23:24:04 -0400

    game loop! refer to the square class in GameMap to implement enemy movement

--- DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/viewmodels/GameMap.java
+++ DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/viewmodels/GameMap.java
@@ -1,6 +1,5 @@
 package com.example.dungeoncrawling.viewmodels;
 
-import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
@@ -8,11 +7,11 @@ import android.view.SurfaceHolder;
 
 import androidx.annotation.NonNull;
 
-import com.example.dungeoncrawling.model.DirectionStrategy;
 import com.example.dungeoncrawling.model.WallCheck;
 import com.example.dungeoncrawling.model.graphics.SpriteSheet;
 import com.example.dungeoncrawling.model.map.Tilemap;
 import com.example.dungeoncrawling.model.Player;
+import com.example.dungeoncrawling.model.map.MapLayout;
 
 public class GameMap implements SurfaceHolder.Callback {
     private SurfaceHolder holder;
@@ -20,6 +19,8 @@ public class GameMap implements SurfaceHolder.Callback {
     private Paint white;
     private Player player;
     private WallCheck wallCheck;
+    private GameLoop gameLoop;
+    private Square sq;
 
     public GameMap(SurfaceHolder holder, SpriteSheet spriteSheet, int roomInd) {
         this.holder = holder;
@@ -33,28 +34,30 @@ public class GameMap implements SurfaceHolder.Callback {
 
         this.wallCheck = new WallCheck(this.tilemap);
         this.wallCheck.subscribe(this.player, this.player.getRow(), this.player.getCol());
+
+        this.gameLoop = new GameLoop(this);
+
+        this.sq = new Square(1, 1);
     }
 
     public void render() {
         Canvas c = this.holder.lockCanvas();
-        c.drawRect(new Rect(0, 0, 4000, 1000), white);
-        this.tilemap.draw(c);
-        this.player.draw(c);
-        this.holder.unlockCanvasAndPost(c);
+        if (c != null) {
+            c.drawRect(new Rect(0, 0, 4000, 1000), white);
+            this.tilemap.draw(c);
+            this.player.draw(c);
+            this.sq.draw(c);
+            this.holder.unlockCanvasAndPost(c);
+        }
     }
 
-    public boolean update(DirectionStrategy strat) {
-        int[] newLoc = strat.move(this.player);
-        this.wallCheck.check(newLoc[0], newLoc[1]);
-
-        if (this.tilemap.isExit(this.player.getRow(), this.player.getCol())) {
-            return true;
-        }
-        return false;
+    public void update() {
+        this.sq.move();
     }
 
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+        this.gameLoop.startGameLoop();
         render();
     }
 
@@ -67,4 +70,38 @@ public class GameMap implements SurfaceHolder.Callback {
     public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
 
     }
+}
+
+class Square {
+    private int row;
+    private int col;
+    private int xDir = 1, yDir = 1;
+    private int width = 64;
+    private Paint white;
+
+    public Square(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.white = new Paint();
+        this.white.setColor(-1);
+    }
+
+    public void move() {
+        this.row += xDir;
+        if (this.row >= MapLayout.NUM_ROWS - 1 || this.row < 0) {
+            xDir *= -1;
+        }
+
+        this.col += yDir;
+        if (this.col >= MapLayout.NUM_COLS - 1 || this.col < 0) {
+            yDir *= -1;
+        }
+    }
+
+    public void draw(Canvas c) {
+        int x = this.col * MapLayout.TILE_WIDTH;
+        int y = this.row * MapLayout.TILE_HEIGHT + 256;
+        c.drawRect(new Rect(x, y, x+MapLayout.TILE_WIDTH, y+MapLayout.TILE_HEIGHT), this.white);
+    }
+
 }
