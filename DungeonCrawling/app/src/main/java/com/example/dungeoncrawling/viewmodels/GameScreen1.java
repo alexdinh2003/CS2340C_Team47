2e069109b3f3746bafaedb378df45d092e2bda73
mjones464 <mjones464@gatech.edu>
2023-11-19 12:26:22 -0500

    adds attack button and checks to see if enemy is next to player

--- DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/viewmodels/GameScreen1.java
+++ DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/viewmodels/GameScreen1.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -60,10 +61,11 @@ public class GameScreen1 extends AppCompatActivity {
     private MediaPlayer player1;
     private Button stopMusic;
     private Button startMusic;
+    private Button attack;
 
     /** @noinspection checkstyle:MissingSwitchDefault*/
     /** @noinspection checkstyle:MissingSwitchDefault, checkstyle:MethodLength */
-    @SuppressLint("SetTextI18n")
+    @SuppressLint({"SetTextI18n", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -94,6 +96,9 @@ public class GameScreen1 extends AppCompatActivity {
         right = findViewById(R.id.right);
         up = findViewById(R.id.up);
         down = findViewById(R.id.down);
+
+        //button for attack
+        attack = findViewById(R.id.attack);
 
         //run/restart timer
         timer = new Timer(System.currentTimeMillis(), timerText, scoreText);
@@ -170,6 +175,12 @@ public class GameScreen1 extends AppCompatActivity {
 
         startMusic.setOnClickListener(v -> {
             play();
+        });
+
+        //player attack
+        attack.setOnClickListener(v -> {
+            int[] pos = player.getPosition();
+            map.playerAttack(pos);
         });
     }
 
