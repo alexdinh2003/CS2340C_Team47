5406d5826b5b370576f5b19f4989cac8928e2eb1
snehayourpal <snehapal205@gmail.com>
2023-11-20 16:36:20 -0500

    Collision Sound

--- DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/model/enemies/EnemyPlayerCollision.java
+++ DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/model/enemies/EnemyPlayerCollision.java
@@ -1,6 +1,9 @@
 package com.example.dungeoncrawling.model.enemies;
 
 import com.example.dungeoncrawling.model.Subscriber;
+import android.content.Context;
+import android.media.MediaPlayer;
+import com.example.dungeoncrawling.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,34 +12,41 @@ public class EnemyPlayerCollision {
     private List<EnemySubscriber> subscribers = new ArrayList<>();
     private int row;
     private int col;
+    private MediaPlayer collisionSound; // New field for MediaPlayer
 
-    public EnemyPlayerCollision(int row, int col) {
+    public EnemyPlayerCollision(Context context, int row, int col) {
         this.row = row;
         this.col = col;
+        collisionSound = MediaPlayer.create(context, R.raw.collision_sound); // Initialize with your collision sound
     }
 
     public void check(int newRow, int newCol) {
         this.row = newRow;
         this.col = newCol;
         notifySubscribers();
+        playCollisionSound(); // Play collision sound
+    }
+
+    private void playCollisionSound() {
+        if (collisionSound != null) {
+            collisionSound.start();
+        }
     }
 
     public void subscribe(EnemySubscriber subscriber) {
         subscribers.add(subscriber);
     }
+
     public void unsubscribe(Subscriber subscriber) {
         subscribers.remove(subscriber);
     }
 
     public void unsubscribeAll() {
-        for (int i = 0; i < subscribers.size(); i++) {
-            subscribers.remove(i);
-            i--;
-        }
+        subscribers.clear();
     }
 
     public void removeAll() {
-        subscribers = new ArrayList<EnemySubscriber>();
+        subscribers = new ArrayList<>();
     }
 
     public int getRow() {
@@ -54,6 +64,13 @@ public class EnemyPlayerCollision {
     protected void notifySubscribers() {
         for (EnemySubscriber subscriber : subscribers) {
             subscriber.update(this);
+        }
+    }
+
+    public void release() {
+        if (collisionSound != null) {
+            collisionSound.release();
+            collisionSound = null;
         }
     }
 }
