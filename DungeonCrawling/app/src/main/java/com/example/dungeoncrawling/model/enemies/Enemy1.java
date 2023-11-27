216fa22d69b831a9a1d6ceac7fa8d0d89f78f8ec
Li-Erik <lierik301@gmail.com>
2023-11-27 16:35:42 -0500

    documentation added

--- DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/model/enemies/Enemy1.java
+++ DungeonCrawling/app/src/main/java/com/example/dungeoncrawling/model/enemies/Enemy1.java
@@ -22,7 +22,7 @@ public class Enemy1 extends Enemy implements EnemySubscriber {
         this.sprite = this.getSpriteSheet().getEnemy(0);
     }
 
-    //moves left and right
+    //moves up and down
     public void move() {
         //change position in some way every 1/2 sec
         if (moveCount > 24) {
