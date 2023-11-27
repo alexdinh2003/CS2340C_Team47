4662ec9128ef31226c6f727fed2e9ba2bbf45b22
snehayourpal <snehapal205@gmail.com>
2023-11-27 16:04:17 -0500

    PlayerAttackTest

---
+++ DungeonCrawling/app/src/test/java/com/example/dungeoncrawling/PlayerAttackTest.java
@@ -0,0 +1,73 @@
+package com.example.dungeoncrawling;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.example.dungeoncrawling.model.enemies.Enemy;
+import com.example.dungeoncrawling.model.enemies.EnemyFactory;
+import com.example.dungeoncrawling.viewmodels.GameMap;
+import com.example.dungeoncrawling.model.graphics.SpriteSheet;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerAttackTest {
+
+    private GameMap gameMap;
+    private SurfaceHolder holder;
+    private Context context;
+
+    @Before
+    public void setUp() {
+        SpriteSheet spriteSheet = new SpriteSheet(context);
+        int roomIndex = 0;
+
+        SurfaceView surface = new SurfaceView(context);
+        holder = surface.getHolder();
+
+        gameMap = new GameMap(holder, spriteSheet, roomIndex, context);
+    }
+
+    @Test
+    public void playerAttackRemovesEnemy() {
+        // Arrange
+        int initialPlayerRow = 1;
+        int initialPlayerCol = 1;
+        int enemyRow = 2;
+        int enemyCol = 2;
+
+        // Create an enemy at the specified position
+        Enemy enemy = EnemyFactory.getEnemy("enemy1", enemyRow, enemyCol);
+        gameMap.getEnemies().add(enemy);
+
+        // Act
+        gameMap.playerAttack(new int[]{initialPlayerRow, initialPlayerCol});
+
+        // Assert
+        assertEquals(0, gameMap.getEnemies().size()); // Enemy should be removed
+        assertEquals(5, gameMap.getPlayer().getScore()); // Score should be incremented
+    }
+
+    @Test
+    public void playerAttackDoesNotRemoveEnemyIfOutOfRange() {
+        // Arrange
+        int initialPlayerRow = 1;
+        int initialPlayerCol = 1;
+        int enemyRow = 5;
+        int enemyCol = 5;
+
+        // Create an enemy at the specified position
+        Enemy enemy = EnemyFactory.getEnemy("enemy1", enemyRow, enemyCol);
+        gameMap.getEnemies().add(enemy);
+
+        // Act
+        gameMap.playerAttack(new int[]{initialPlayerRow, initialPlayerCol});
+
+        // Assert
+        assertEquals(1, gameMap.getEnemies().size()); // Enemy should not be removed
+        assertEquals(0, gameMap.getPlayer().getScore()); // Score should not be incremented
+    }
+}
